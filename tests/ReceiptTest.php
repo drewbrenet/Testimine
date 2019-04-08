6b2b1a2d8269d9ed62c234278397243fd70ff1eb
brenet <brenetoispuu@gmail.com>
2019-04-08 10:02:09 +0300

    Add comments

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -5,12 +5,18 @@ require("vendor\autoload.php");
 use PHPUnit\Framework\TestCase;
 use TDD\Receipt;
 
+// Class that extends TestCase
 class ReceiptTest extends TestCase {
     public function testTotal() {
+        // Object
         $Receipt = new Receipt();
+        // Test method
         $this->assertEquals(
+            // Expected value
             15,
+            //Total value
             $Receipt->total([0,2,5,8]),
+            //Error message
             'When summing the total should equal 15'
         );
     }
