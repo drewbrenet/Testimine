9a6c4b9c3896abd48308006384fdf1c6c0333b33
brenet <brenetoispuu@gmail.com>
2019-04-08 10:44:54 +0300

    Test tax method

--- src/Receipt.php
+++ src/Receipt.php
@@ -4,4 +4,7 @@ class Receipt {
     public function total(array $item = []) {
         return array_sum($item);
     }
+    public function tax($ammount, $tax){
+       return ($ammount * $tax);
+    }
 }
--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -7,17 +7,39 @@ use TDD\Receipt;
 
 // Class that extends TestCase
 class ReceiptTest extends TestCase {
+    public function setUp() {
+       $this->Receipt = new Receipt();
+    }
+
+    public function setDown() {
+        unset($this->Receipt);
+    }
     public function testTotal() {
-        // Object
-        $Receipt = new Receipt();
+        // Input
+        $input = ([0,2,5,8]);
+        $output = $this->Receipt->total($input);
         // Test method
         $this->assertEquals(
             // Expected value
             15,
-            //Total value
-            $Receipt->total([0,2,5,8]),
+            $output,
             //Error message
             'When summing the total should equal 15'
+        );
+    }
+
+    public function testTax() {
+        // Add values to input
+        $inputAmount = 10.00;
+        $taxInput = 0.10;
+        // Add value for output
+        $output = $this->Receipt->tax($inputAmount, $taxInput);
+        // Test method
+        $this->assertEquals(
+            1.00,
+            $output,
+            //Error message
+            'Tax should equal 1.00'
         );
     }
 }
